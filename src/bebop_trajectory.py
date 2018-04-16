a94b2106e4f63cbbadc7a8ed25856f78bc48c2e2
Bojan <bojan.spahija@gmail.com>
2018-04-16 16:55:50 +0200

    Trajectory msg handling

--- src/bebop_trajectory.py
+++ src/bebop_trajectory.py
@@ -4,7 +4,13 @@
 import rospy
 import math
 import scipy
+import numpy as np
+from geometry_msgs.msg import Vector3
+from mav_msgs.msg import Actuators
+from nav_msgs.msg import Odometry
+from std_msgs.msg import Float64
 from trajectory_msgs.msg import MultiDOFJointTrajectory
+
 
 class BebopTrajectory:
 
@@ -25,8 +31,14 @@ class BebopTrajectory:
 
         self.arm = self.l * math.cos(math.pi / 4)
 
-        # True if node recieved bebop trajectory, otherwise false
-        self.trajectory_recieved = False
+        # Define magic thrust number :-)
+        self.magic_number = 0.908
+
+        # Counter
+        self.i = -1
+
+        # True if node received bebop trajectory, otherwise false
+        self.trajectory_received = False
         self.first_measurement = False
         self.controller_info = False
 
@@ -46,6 +58,10 @@ class BebopTrajectory:
             "bebop/angle_ref",
             Vector3,
             self.angle_cb)
+        self.trajectory_subscriber = rospy.Subscriber(
+            "/bebop/trajectory_reference",
+            MultiDOFJointTrajectory,
+            self.trajectory_cb)
 
         # initialize publishers
         self.motor_pub = rospy.Publisher(
@@ -63,7 +79,7 @@ class BebopTrajectory:
 
         self.actuator_msg = Actuators()
 
-        # define vector for measured and setopint values
+        # define vector for measured and setpoint values
         self.pose_sp = Vector3(0., 0., 0.)
         self.euler_sp = Vector3(0., 0., 0.)
         self.euler_mv = Vector3(0., 0., 0.)
@@ -71,10 +87,81 @@ class BebopTrajectory:
         self.p = 0
         self.q = 0
         self.r = 0
+        self.trajectory_points = []
+        self.actuator_msg.angular_velocities = \
+            [self.hover_speed, self.hover_speed, self.hover_speed, self.hover_speed]
         self.t_old = 0
 
-        # define PID for height control
-        self.z_mv = 0
+        # Controller rate
+        # Trajectory points are being given with a frequency of 100Hz
+        self.controller_rate = 100
+        self.rate = rospy.Rate(self.controller_rate)
+
+    def setpoint_cb(self, data):
+
+        self.pose_sp.x = data.x
+        self.pose_sp.y = data.y
+        self.pose_sp.z = data.z
+
+    def angle_cb(self, data):
+
+        self.euler_sp = Vector3(data.x, data.y, data.z)
+
+    def odometry_callback(self, data):
+        """Callback function for odometry subscriber"""
+
+        self.first_measurement = True
+
+        self.x_mv = data.pose.pose.position.x
+        self.y_mv = data.pose.pose.position.y
+        self.z_mv = data.pose.pose.position.z
+
+        self.vx_mv = data.twist.twist.linear.x
+        self.vy_mv = data.twist.twist.linear.y
+        self.vz_mv = data.twist.twist.linear.z
+
+        self.p = data.twist.twist.angular.x
+        self.q = data.twist.twist.angular.y
+        self.r = data.twist.twist.angular.z
+
+        self.qx = data.pose.pose.orientation.x
+        self.qy = data.pose.pose.orientation.y
+        self.qz = data.pose.pose.orientation.z
+        self.qw = data.pose.pose.orientation.w
+
+    def odometry_gt_callback(self, data):
+        self.x_gt_mv = data.pose.pose.position.x
+        self.y_gt_mv = data.pose.pose.position.y
+        self.z_gt_mv = data.pose.pose.position.z
+
+    def trajectory_cb(self, data):
+        self.trajectory_received = True
+        self.trajectory_points = data.points
+
+    def convert_to_euler(self, qx, qy, qz, qw):
+        """Calculate roll, pitch and yaw angles/rates with quaternions"""
+
+        # conversion quaternion to euler (yaw - pitch - roll)
+        self.euler_mv.x = math.atan2(2 * (qw * qx + qy * qz), qw * qw
+                                     - qx * qx - qy * qy + qz * qz)
+        self.euler_mv.y = math.asin(2 * (qw * qy - qx * qz))
+        self.euler_mv.z = math.atan2(2 * (qw * qz + qx * qy), qw * qw
+                                     + qx * qx - qy * qy - qz * qz)
+
+        # gyro measurements (p,q,r)
+        p = self.p
+        q = self.q
+        r = self.r
+
+        sx = math.sin(self.euler_mv.x)  # sin(roll)
+        cx = math.cos(self.euler_mv.x)  # cos(roll)
+        cy = math.cos(self.euler_mv.y)  # cos(pitch)
+        ty = math.tan(self.euler_mv.y)  # cos(pitch)
+
+        # conversion gyro measurements to roll_rate, pitch_rate, yaw_rate
+        self.euler_rate_mv.x = p + sx * ty * q + cx * ty * r
+        self.euler_rate_mv.y = cx * q - sx * r
+        self.euler_rate_mv.z = sx / cy * q + cx / cy * r
 
     def dlqr(self, Q, R):
         """Solve the discrete time lqr controller.
@@ -131,31 +218,31 @@ class BebopTrajectory:
         gama17 = a3 * q
         gama18 = a3 * p
 
-        A = [[0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-             [0, 0, 0, 0, 0, 0, gama1, 0, gama2, 0, gama3, 0],
-             [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-             [0, 0, 0, 0, 0, 0, gama4, 0, gama5, 0, gama6, 0],
-             [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
-             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-             [0, 0, 0, 0, 0, 0, 0, 1, 0, gama7, 0, gama8],
-             [0, 0, 0, 0, 0, 0, 0, 0, 0, gama9, 0, gama10],
-             [0, 0, 0, 0, 0, 0, 0, 0, 0, gama11, 0, gama12],
-             [0, 0, 0, 0, 0, 0, 0, gama13, 0, 0, 0, gama14],
-             [0, 0, 0, 0, 0, 0, 0, 0, 0, gama15, 0, gama16],
-             [0, 0, 0, 0, 0, 0, 0, gama17, 0, gama18, 0, 0]]
+        A = np.matrix([[0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                      [0, 0, 0, 0, 0, 0, gama1, 0, gama2, 0, gama3, 0],
+                      [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+                      [0, 0, 0, 0, 0, 0, gama4, 0, gama5, 0, gama6, 0],
+                      [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+                      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                      [0, 0, 0, 0, 0, 0, 0, 1, 0, gama7, 0, gama8],
+                      [0, 0, 0, 0, 0, 0, 0, 0, 0, gama9, 0, gama10],
+                      [0, 0, 0, 0, 0, 0, 0, 0, 0, gama11, 0, gama12],
+                      [0, 0, 0, 0, 0, 0, 0, gama13, 0, 0, 0, gama14],
+                      [0, 0, 0, 0, 0, 0, 0, 0, 0, gama15, 0, gama16],
+                      [0, 0, 0, 0, 0, 0, 0, gama17, 0, gama18, 0, 0]])
 
-        B = [[0, 0, 0, 0],
-             [c1, 0, 0, 0],
-             [0, 0, 0, 0],
-             [c2, 0, 0, 0],
-             [0, 0, 0, 0],
-             [c3, 0, 0, 0],
-             [0, 0, 0, 0],
-             [0, b1, 0, 0],
-             [0, 0, 0, 0],
-             [0, 0, b2, 0],
-             [0, 0, 0, 0],
-             [0, 0, 0, b3]]
+        B = np.matrix([[0, 0, 0, 0],
+                      [c1, 0, 0, 0],
+                      [0, 0, 0, 0],
+                      [c2, 0, 0, 0],
+                      [0, 0, 0, 0],
+                      [c3, 0, 0, 0],
+                      [0, 0, 0, 0],
+                      [0, b1, 0, 0],
+                      [0, 0, 0, 0],
+                      [0, 0, b2, 0],
+                      [0, 0, 0, 0],
+                      [0, 0, 0, b3]])
 
         # first, try to solve the Ricatti equation
         X = np.matrix(scipy.linalg.solve_discrete_are(A, B, Q, R))
@@ -168,7 +255,59 @@ class BebopTrajectory:
         return K, X, eigVals
 
     def run(self):
-        pass
+        """ Run ROS node - computes motor speed for trajectory following using LQR algorithm """
+
+        while not self.first_measurement:
+            print("LaunchBebop.run() - Waiting for first measurement.")
+            rospy.sleep(self.sleep_sec)
+
+        print("LaunchBebop.run() - Starting position control")
+        self.t_old = rospy.Time.now()
+
+        while not rospy.is_shutdown():
+            self.rate.sleep()
+
+            t = rospy.Time.now()
+            dt = (t - self.t_old).to_sec()
+            self.t_old = t
+
+            if dt < 0.99 / self.controller_rate:
+                continue
+
+            self.convert_to_euler(self.qx, self.qy, self.qz, self.qw)
+
+            motor_speed1 = self.hover_speed
+            motor_speed2 = self.hover_speed
+            motor_speed3 = self.hover_speed
+            motor_speed4 = self.hover_speed
+            self.actuator_msg.angular_velocities = \
+                [self.magic_number * motor_speed1, self.magic_number * motor_speed2,
+                 motor_speed3, motor_speed4]
+
+            if self.trajectory_received:
+                self.i = 0
+                self.trajectory_received = False
+
+            if self.i >= 0:
+                self.pose_sp = self.trajectory_points[self.i].transforms[0].translation
+                quart_sp = self.trajectory_points[self.i].transforms[0].rotation
+                self.euler_sp = self.convert_to_euler(quart_sp.x, quart_sp.y, quart_sp.z, quart_sp.w)
+
+
+            # Print out controller information
+            if self.controller_info:
+                print(dt)
+                print("Comparison x:{}\nx_m:{}\ny:{}\ny_m:{}\nz:{}\nz_m{}".format(
+                    self.pose_sp.x,
+                    self.x_mv,
+                    self.pose_sp.y,
+                    self.y_mv,
+                    self.pose_sp.z,
+                    self.z_mv))
+                print("Motor speeds are {}".format(self.actuator_msg.angular_velocities))
+                print("Current quadcopter height is: {}".format(self.z_mv))
+
+            self.motor_pub.publish(self.actuator_msg)
 
 
 if __name__ == '__main__':
